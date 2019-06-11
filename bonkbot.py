0a4cc77e37647c6f53d26c4bdabf150e09251f33
o2bomb <felixtan.tech@gmail.com>
2019-06-11 23:48:09 +0800

    Add global error handling and usage instructions for commands

--- bonkbot.py
+++ bonkbot.py
@@ -30,27 +30,33 @@ async def kill(ctx):
         await bot.close()
 
 
-@bot.command()
+@bot.command(usage="bonk.tts source [language] [speed]")
 async def tts(ctx, source, language="en-us", speed="0"):
     # @Improve
-    v_state = ctx.author.voice
-    if len(source) > 60:
-        await ctx.send("Source cannot be longer than 60 characters. This is to reduce bandwidth.")
-    elif v_state is not None:    # If the author of the command is in a voice channel
-        if bot.user not in v_state.channel.members:
-            v_client = await v_state.channel.connect()
+    try:
+        v_state = ctx.author.voice
+        if len(source) > 60:
+            await ctx.send("Source cannot be longer than 60 characters. This is to reduce memory usage and bandwidth.")
+        elif v_state is not None:    # If the author of the command is in a voice channel
+            if bot.user not in v_state.channel.members:
+                v_client = await v_state.channel.connect()
+            else:
+                for v_client in bot.voice_clients:
+                    if v_client == v_state.channel:
+                        break
+            texttospeech.get_tts(source, language, speed)
+            audio_src = discord.FFmpegPCMAudio('temp.mp3')
+            v_client.play(audio_src)
         else:
-            for v_client in bot.voice_clients:
-                if v_client == v_state.channel:
-                    break
-        texttospeech.get_tts(source, language, speed)
-        audio_src = discord.FFmpegPCMAudio('temp.mp3')
-        v_client.play(audio_src)
-    else:
-        print("Error: TTS failed. Author is not in a voice channel.")
+            print("Error: TTS failed. Author is not in a voice channel.")
+    except (discord.ClientException) as e:
+        if e == discord.ClientException:
+            ctx.send("Please slow down. I cannot play more than one track at a time.")
+        else:
+            raise e
 
 
-@bot.command()
+@bot.command(usage="bonk.forecast city [country]")
 async def forecast(ctx, city, country=""):
     # @Improve
     # Gets the current forecast for the specified city and bulids
@@ -62,6 +68,32 @@ async def forecast(ctx, city, country=""):
     forecast_embed.add_field(name=f"{result['weather'][0]['main']}", value=f"{result['weather'][0]['description']}", inline=True)
     forecast_embed.set_footer(text="Weather data provided by OpenWeatherMaps")
     await ctx.send(embed=forecast_embed)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    # @Update
+    # Handles all command related errors (errors that are raised when a command fails)
+    ignored_err = ()
+
+    if isinstance(error, ignored_err):
+        return
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(("I cannot execute that because there are missing parameters. The correct usage for the command is:\n"
+                        f"`{ctx.command.usage}`"))
+    elif isinstance(error, commands.CommandNotFound):
+        prediction = None
+        invoked = ctx.message.content.split()
+        for command in bot.commands:
+            if invoked[0][5:] in command.name:
+                prediction = command.name
+
+        if prediction is not None:
+            await ctx.send(f"That command does not exist. Did you mean `bonk.{prediction}`")
+        else:
+            await ctx.send(f"That command does not exist.")
+    elif isinstance(error, commands.DisabledCommand):
+        await ctx.send("That command has been disabled.")
 
 
 @bot.event
